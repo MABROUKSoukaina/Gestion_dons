cb39aefbf5f9457942f6a346597a94c6d496f15c
MABROUKSoukaina <soukaynamabrouk20@gmail.com>
2023-12-22 09:33:21 +0100

    Premier commi

--- components/MapScreen.jsx
+++ components/MapScreen.jsx
@@ -1,76 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { View, StyleSheet, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import axios from 'axios';
 
 export default function MapScreen() {
-  const [xCoordinates, setXCoordinates] = useState([]);
-  const [yCoordinates, setYCoordinates] = useState([]);
+  const [communeData, setCommuneData] = useState([]);
 
   useEffect(() => {
-    const fetchXCoordinates = async () => {
+    const fetchData = async () => {
       try {
-        const responseX = await axios.get('http://172.16.26.120:8083/Commune/getX');
-        if (responseX.data && Array.isArray(responseX.data)) {
-          setXCoordinates(responseX.data);
-          console.log('Coordonnées Y récupérées avec succès :', responseX.data);
+        const response = await axios.get('http://172.16.220.122:8083/Commune/affannonce');
+        if (response.data && Array.isArray(response.data)) {
+          setCommuneData(response.data);
+          console.log('Données de commune récupérées avec succès :', response.data);
         }
       } catch (error) {
-        console.error('Erreur lors de la récupération des coordonnées X:', error);
+        console.error('Erreur lors de la récupération des données de commune :', error);
       }
     };
 
-    const fetchYCoordinates = async () => {
-      try {
-        const responseY = await axios.get('http://172.16.26.120:8083/Commune/getY');
-        if (responseY.data && Array.isArray(responseY.data)) {
-          setYCoordinates(responseY.data);
-          console.log('Coordonnées Y récupérées avec succès :', responseY.data);
-        }
-      } catch (error) {
-        console.error('Erreur lors de la récupération des coordonnées Y:', error);
-      }
-    };
-
-    // Appel des deux fonctions pour récupérer les données
-    fetchXCoordinates();
-    fetchYCoordinates();
+    // Appel de la fonction pour récupérer les données de commune
+    fetchData();
   }, []);
 
-  // Génération des marqueurs à partir des coordonnées X et Y
-  const generateMarkersFromCoordinates = () => {
-    const markers = [];
-    for (let i = 0; i < Math.min(xCoordinates.length, yCoordinates.length); i++) {
-      markers.push({
-        title: `Marker ${i + 1}`,
-        coordinates: {
-          latitude: yCoordinates[i],
-          longitude: xCoordinates[i],
-        },
-      });
-    }
-    return markers;
+  // Génération des marqueurs à partir des données de commune
+  const generateMarkersFromCommuneData = () => {
+    return communeData.map((commune, index) => ({
+      key: index.toString(),
+      title: commune[0], // Nom de la commune
+      coordinate: {
+        latitude: commune[2], // Coordonnée Y (latitude)
+        longitude: commune[1], // Coordonnée X (longitude)
+      },
+      nbrAnnonce: commune[3], // Nombre d'annonces
+    }));
   };
 
-  const markers = generateMarkersFromCoordinates();
+  const markers = generateMarkersFromCommuneData();
 
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-         initialRegion={{
+        initialRegion={{
           latitude: 31.7917, // Latitude centrée sur le Maroc
           longitude: -7.0926, // Longitude centrée sur le Maroc
           latitudeDelta: 10, // Valeur arbitraire pour afficher une région étendue du Maroc
           longitudeDelta: 10, // Valeur arbitraire pour afficher une région étendue du Maroc
         }}
       >
-        {markers.map((marker, index) => (
+        {markers.map((marker) => (
           <Marker
-            key={index}
+            key={marker.key}
             title={marker.title}
-            coordinate={marker.coordinates}
-          />
+            coordinate={marker.coordinate}
+          >
+            <Callout>
+              <View>
+                <Text>{marker.title}</Text>
+                <Text>{`Nombre d'annonces : ${marker.nbrAnnonce}`}</Text>
+              </View>
+            </Callout>
+          </Marker>
         ))}
       </MapView>
     </View>
